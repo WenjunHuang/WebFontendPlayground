1ccdb5f12c89ebf53eea835250081f002a0b1dc3
WenjunHuang <wenjun.huang80@gmail.com>
2021-06-22 18:03:57 +0800

    update

--- src/client/elm/PhotoGroove/Main.elm
+++ src/client/elm/PhotoGroove/Main.elm
@@ -1,10 +1,10 @@
 module PhotoGroove.Main exposing (main)
 
-import Array exposing (Array)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Http
 import Random
 
 
@@ -30,6 +30,7 @@ type Msg
     | ClickedSize ThumbnailSize
     | ClickedSurpriseMe
     | GotRandomPhoto Photo
+    | GotPhotos (Result Http.Error String)
 
 
 type Status
@@ -159,6 +160,23 @@ update msg model =
 
         GotRandomPhoto photo ->
             ( { model | status = selectUrl photo.url model.status }, Cmd.none )
+
+        GotPhotos result ->
+            case result of
+                Ok responseStr ->
+                    case String.split "," responseStr of
+                        (firstUrl :: _) as urls ->
+                            let
+                                photos =
+                                    List.map (\url -> { url = url }) urls
+                            in
+                            ( { model | status = Loaded photos firstUrl }, Cmd.none )
+
+                        [] ->
+                            ( { model | status = Errored "0 photos found" }, Cmd.none )
+
+                Err httpError ->
+                    ( { model | status = Errored "Server error!" }, Cmd.none )
 
 
 main : Program () Model Msg
